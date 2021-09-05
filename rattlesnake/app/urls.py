112dd989e6a03e9e91a6479432a2f8f5491d82fc
Naveen <naveenterance@gmail.com>
2021-09-05 09:22:21 +0530

    Fifth

--- rattlesnake/app/urls.py
+++ rattlesnake/app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from app.views import MyView
 
 urlpatterns = [
     path('', views.NotesList.as_view(), name='notes_list'),
@@ -7,4 +8,7 @@ urlpatterns = [
     path('new', views.NotesCreate.as_view(), name='notes_new'),
     path('edit/<int:pk>', views.NotesUpdate.as_view(), name='notes_edit'),
     path('delete/<int:pk>', views.NotesDelete.as_view(), name='notes_delete'),
+
+    path('log', MyView.as_view(), name='log'),
+
 ]
--- rattlesnake/app/views.py
+++ rattlesnake/app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-
+from django.views import View
 from .models import Notes
 
 # Create your views here.
@@ -29,3 +29,30 @@ class NotesUpdate(UpdateView):
 class NotesDelete(DeleteView):
     model = Notes
     success_url = reverse_lazy('notes_list')
+
+class MyView(View):
+
+    def get(self, request, *args, **kwargs):
+        insert='INSERT'
+        delete='DELETE'
+
+        f = open("debug.log", "r")
+
+
+        line_number = 0
+        list_of_results = []
+
+        # Open the file in read only mode
+        with open("debug.log", "r") as read_obj:
+            # Read all lines in the file one by one
+            for line in read_obj:
+                # For each line, check if line contains the string
+                line_number += 1
+                if insert in line:
+                # If yes, then add the line number & line as a tuple in the list
+                    list_of_results.append(("<h1>Added<h1>",line.rstrip()))
+                if delete in line:
+                    list_of_results.append(("<h1>Deleted<h1>",line.rstrip()))
+            # Return list of tuples containing line numbers and lines where string is found
+        return HttpResponse("<h5>%s<h5>"%list_of_results)
+               
